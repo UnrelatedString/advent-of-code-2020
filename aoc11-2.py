3b29691924cb75bae8f1f20e83f73fcbbc0742b2
Kai Hoop <unrelatedstring@gmail.com>
2020-12-11 00:21:22 -0500

    First placement in the 200s in a while

--- aoc11-2.py
+++ aoc11-2.py
@@ -10,11 +10,24 @@ ss = []
 s = [r[:] for r in ls]
 def get0(x,y,l):
     if y < 0 or y >= len(l) or x < 0 or x >= len(l[y]):
-        return False
+        return 'L'
     else:
         return l[y][x]
+    
+def getd(x,y,X,Y,l):
+    x += X
+    y += Y
+    while get0(x,y,l) == '.':
+        x += X
+        y += Y
+    return get0(x,y,l)
+        
 def mn(x,y,l):
-    return sum(get0(x+X,y+Y,l)=='#' for X in [-1,0,1] for Y in [-1,0,1])
+    n = 0
+    for X in [-1,0,1]:
+        for Y in [-1,0,1]:
+            n += getd(x,y,X,Y,l) == '#'
+    return n
 while s not in ss:
     ss.append(s)
     n = [r[:] for r in s]
@@ -23,7 +36,7 @@ while s not in ss:
             if c == 'L':
                 n[y][x] = 'L#'[mn(x,y,s)==0]
             elif c == '#':
-                n[y][x] = 'L#'[mn(x,y,s)<5] #inclusive
+                n[y][x] = 'L#'[mn(x,y,s)<6] #inclusive
     s = n
 
 print(sum(r.count('#') for r in s))
