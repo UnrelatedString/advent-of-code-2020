85ee2214c7a38d7afe276a27e43bdf57e73143b6
Kai Hoop <unrelatedstring@gmail.com>
2020-12-25 00:18:02 -0500

    this was like genuinely embarrassing

--- aoc25.py
+++ aoc25.py
@@ -3,9 +3,14 @@ from functools import *
 import re
 from copy import deepcopy
 
-ls = [l for l in iter(input,'e')]
+ls = [int(l) for l in iter(input,'')]
 
 #lgs = '\n'.join(ls).rstrip('\n').split('\n\n')
 
+mod = 20201227
 
-    
+xs = set()
+for x in count(1):
+    if pow(7,x,mod) in ls:
+        ls.remove(pow(7,x,mod))
+        print(pow(ls.pop(),x,mod))
